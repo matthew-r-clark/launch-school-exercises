f8724eb2a6f56b0dede83f3ddb173a68e299e115
Matt Clark <clark.matthewr@gmail.com>
2019-07-09 16:34:36 -0500

    protein translation

---
+++ challenges/med1/protein_translation.rb
@@ -0,0 +1,21 @@
+class Translation
+  AMINOS = {
+    AUG: "Methionine", UUU: "Phenylalanine", UUC: "Phenylalanine",
+    UUA: "Leucine", UUG: "Leucine", UCU: "Serine", UCC: "Serine",
+    UCA: "Serine", UCG: "Serine", UAU: "Tyrosine", UAC: "Tyrosine",
+    UGU: "Cysteine", UGC: "Cysteine", UGG: "Tryptophan",
+    UAA: "STOP", UAG: "STOP", UGA: "STOP"
+  }
+
+  def self.of_codon(codon)
+    AMINOS.fetch(codon.to_sym) {fail InvalidCodonError}
+  end
+
+  def self.of_rna(strand)
+    strand.scan(/.../)
+      .take_while {|codon| of_codon(codon) != "STOP"}
+      .map {|codon| of_codon(codon)}
+  end
+end
+
+class InvalidCodonError < StandardError; end
