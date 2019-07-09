1fcaea9ef35f65655b946abe0991791820889a2a
Matt Clark <clark.matthewr@gmail.com>
2019-07-09 15:09:50 -0500

    beer song

---
+++ challenges/easy2/beer_song.rb
@@ -0,0 +1,47 @@
+module VerseTypes
+  class DefaultVerse
+    def verse(n)
+      "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} bottles of beer on the wall.\n"
+    end
+  end
+  
+  class ZeroVerse
+    def verse(_)
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    end
+  end
+  
+  class OneVerse
+    def verse(_)
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    end
+  end
+  
+  class TwoVerse
+    def verse(_)
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    end
+  end
+end
+
+class BeerSong
+  include VerseTypes
+
+  VERSES = Hash.new(DefaultVerse).merge({
+    0 => ZeroVerse,
+    1 => OneVerse,
+    2 => TwoVerse
+  })
+
+  def verse(n)
+    VERSES[n].new.verse(n)
+  end
+
+  def verses(start, stop)
+    start.downto(stop).map {|n| verse(n)}.join("\n")
+  end
+
+  def lyrics
+    verses(99, 0)
+  end
+end
