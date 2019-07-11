cfe1808f82fb514e8477977d1c74adb2055ff20d
Matt Clark <clark.matthewr@gmail.com>
2019-07-11 10:16:49 -0500

    meetup

---
+++ challenges/med1/meetup.rb
@@ -0,0 +1,34 @@
+require 'date'
+
+class Meetup
+  START_DATE = {
+    first: 1,
+    second: 8,
+    third: 15,
+    fourth: 22,
+    teenth: 13,
+    last: -7
+  }
+
+  def initialize(month, year)
+    @month = month
+    @year = year
+  end
+
+  def day(weekday, schedule)
+    date = Date.new(year, month, START_DATE[schedule])
+    
+    7.times do
+      return date if date.send(add_question_mark_to_sym(weekday))
+      date = date.next
+    end
+  end
+
+  def add_question_mark_to_sym(weekday)
+    (weekday.to_s + "?").to_sym
+  end
+
+  private
+
+  attr_reader :month, :year
+end
